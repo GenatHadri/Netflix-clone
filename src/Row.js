f294c50ccaa910dde00d02946986986fb61a702c
Genat Hadri <gh43836@ubt-uni.net>
2022-01-06 18:45:17 +0100

    Getting all movies from an API

---
+++ src/Row.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  // A code snippet which runs based on a specific condition/varible
+  useEffect(() => {
+    // if [], run once when the row loads, and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.log(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
